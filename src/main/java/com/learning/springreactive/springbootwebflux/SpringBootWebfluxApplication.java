7ebe0f4e25aeddac7313f73c18ec1882ce1a8dbb
brenda <brenda.nevenka.costa.bojanich@everis.com>
2020-03-21 13:09:09 -0500

    Terminal example to save a document
    - Default and with-args builder added to documento Producto
    - Basic flux created (just)
    - Flatmap to save added

--- src/main/java/com/learning/springreactive/springbootwebflux/SpringBootWebfluxApplication.java
+++ src/main/java/com/learning/springreactive/springbootwebflux/SpringBootWebfluxApplication.java
@@ -1,13 +1,34 @@
 package com.learning.springreactive.springbootwebflux;
 
+import com.learning.springreactive.springbootwebflux.models.dao.ProductoDAO;
+import com.learning.springreactive.springbootwebflux.models.documents.Producto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootWebfluxApplication {
+public class SpringBootWebfluxApplication implements CommandLineRunner {
+
+    @Autowired
+    private ProductoDAO productoDAO;
+
+    private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebfluxApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Flux.just(
+          new Producto("lavadora", 12.5),
+                new Producto("secadora", 10.5),
+                new Producto("peine", 1.0)
+        ).flatMap(producto -> productoDAO.save(producto))
+                .subscribe(producto -> log.info(producto.getId()));
+    }
 }
